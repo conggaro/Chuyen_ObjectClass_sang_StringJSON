6132494b56ab46491d32ad0b1e1567e9b4c505f9
Nguyễn Chí Công <95849309+conggaro@users.noreply.github.com>
2023-08-22 14:10:12 +0700

    gui file len GitHub

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+public class Example
+{
+    public static void Main()
+    {
+        List<Person> people = new List<Person>()
+        {
+            new Person { Name = "John", Age = 42 },
+            new Person { Name = "Jane", Age = 36 },
+            new Person { Name = "Joe", Age = 28 }
+        };
+
+        string jsonString = JsonSerializer.Serialize(people);
+        Console.WriteLine(jsonString);
+    }
+}
